d557cab8c31f89fa35bff5bfcc821fde2e0867da
Alex <alex@codingcat.dev>
2023-04-24 13:23:22 -0400

    include recaptcha v2

--- src/App.svelte
+++ src/App.svelte
@@ -1,83 +1,24 @@
 <script>
-  import Firebase_Logo_Logomark from "./assets/Firebase_Logo_Logomark.svg";
-  import ReCaptcha from "./assets/recaptcha_48dp.png";
-  import { initializeApp } from "firebase/app";
-  import Counter from "./lib/Counter.svelte";
-  import CounterReader from "./lib/CounterReader.svelte";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAH2JT998LjBxxJO0eX_V5LPAyhhxV2fQQ",
-    authDomain: "ccd-firebase-app-check.firebaseapp.com",
-    projectId: "ccd-firebase-app-check",
-    storageBucket: "ccd-firebase-app-check.appspot.com",
-    messagingSenderId: "566747634816",
-    appId: "1:566747634816:web:fec9bcaf25c950d0995a65",
+  import LibLoader from "./lib/LibLoader.svelte";
+  const onLoaded = () => {
+    console.log("third party script loaded");
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // reCAPTCHA App Check
-
-  import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-
-  if (import.meta.env.DEV) {
+  // @ts-ignore
+  window.onloadCallback = () => {
     // @ts-ignore
-    self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-  }
-
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(
-      "6LdAIqQlAAAAAC4kq-bag4J-HmAAVe_pu7T75QOf"
-    ),
-    isTokenAutoRefreshEnabled: true,
-  });
+    grecaptcha.render("html_element", {
+      sitekey: "6LdbU7QlAAAAAFCxSSOngerVPcoZHknumevi7DEC",
+    });
+  };
 </script>
 
-<main>
-  <div>
-    <a href="https://firebase.google.com" target="_blank" rel="noreferrer">
-      <img
-        src={Firebase_Logo_Logomark}
-        class="logo firebase"
-        alt="Firebase Logo"
-      />
-    </a>
-    <a
-      href="https://developers.google.com/recaptcha"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <img src={ReCaptcha} class="logo" alt="Recaptcha Logo" />
-    </a>
-  </div>
-  <h1>Firebase with App Check (using reCAPTCHA)</h1>
+<LibLoader
+  url="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+  on:loaded={onLoaded}
+/>
 
-  <div class="card">
-    <Counter {app} />
-    <CounterReader {app} />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://firebase.google.com/docs/app-check"
-      target="_blank"
-      rel="noreferrer">Firebase App Check</a
-    >
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.firebase:hover {
-    filter: drop-shadow(0 0 2em #ffea00fb);
-  }
-</style>
+<form action="?" method="POST">
+  <div id="html_element" />
+  <br />
+  <input type="submit" value="Submit" />
+</form>
